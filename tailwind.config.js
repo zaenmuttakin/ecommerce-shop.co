76217c246dbfd15ce074da41bf36baecff74cfca
zaenmuttakin <zaenmuttakin@gmail.com>
2023-11-04 00:01:07 +0700

    setup style guide - color, font, icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        satoshi: ["satoshi-variable", "sans-serif"],
+        integral: ["integral-cf-bold", "sans-serif"],
+      }
+    },
+    colors: {
+      black: {
+        10: "#0000001A",
+        20: "#00000033",
+        40: "#00000066",
+        60: "#00000099",
+        100: "#000000",
+      },
+      gray: {
+        10: "#F2F0F1",
+        20: "#F0EEED",
+        30: "#F0F0F0",
+      },
+      red : {
+        10: "#FF33331A",
+        100: "#FF3333",
+      },
+      white: "#FFFFFF",
+      yellow : "#FFC633",
+      green : "#01AB31"
+    }
   },
   plugins: [],
 }
