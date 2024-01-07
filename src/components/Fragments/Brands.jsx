90d6980359be7ca82190184d7f0d99e6a5dbad51
zaenmuttakin <zaenmuttakin@gmail.com>
2024-01-07 09:29:51 +0700

    change flex-wrap on brand component

--- src/components/Fragments/Brands.jsx
+++ src/components/Fragments/Brands.jsx
@@ -2,9 +2,9 @@ import { brandsImage } from "../../assets/images";
 const Brands = () => {
   return (
     <section className="bg-black-100 py-8 xl:py-10">
-      <div className="main-container flex flex-wrap justify-evenly gap-y-5">
+      <div className="main-container flex flex-wrap lg:flex-nowrap justify-evenly gap-y-5">
         {brandsImage.map((image, i) => (
-          <img key={i} src={image} alt={`brands-${i}`} className="h-8 w-1/3 xl:w-fit xl:h-10" />
+          <img key={i} src={image} alt={`brands-${i}`} className="h-8 w-1/3 lg:w-fit xl:h-10" />
         ))}
       </div>
     </section>
